7afc0b0e58f89b8beea8077614bf24fed5fdfba7
Gabriel Miklós <socketbind@gmail.com>
2014-05-24 11:12:45 +0200

    add mirror quickstart

---
+++ mirror/config.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * Copyright (C) 2013 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+//  Author: Jenny Murphy - http://google.com/+JennyMurphy
+
+// TODO: You must configure these fields for the starter project to function.
+// Visit https://developers.google.com/glass/getting-started to learn more
+$api_client_id = "917904597916-qhhj1cev0s6596toai9psf1k5r1hfk8n.apps.googleusercontent.com";
+$api_client_secret = "c48MJSFWbjYAcnLbtkTvFYlm";
+$api_simple_key = "API_KEY";
+
+$base_url = "http://localhost:8000/mirror";
+
+// This should be writable by your web server's user
+$sqlite_database = "/tmp/database.sqlite";
